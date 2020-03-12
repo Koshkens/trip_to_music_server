7005b2159f5a3acf2c1bec539b247f91a63804c7
Koshkens <33977229+Koshkens@users.noreply.github.com>
2020-03-12 20:41:42 +0300

    for heroku

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const clientUrl = '*';
 
 
-app.use(express.static("public"))
+app.get('/', (req, res) => {
+    res.send('OK')
+})
 
 app.listen(process.env.PORT || 8080, () => console.log(`All OK`))
