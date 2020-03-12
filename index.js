3513dee6cfd5ba616e8f3b90132a61bc292d0dfb
Koshkens <33977229+Koshkens@users.noreply.github.com>
2020-03-12 23:45:09 +0300

    test interkassa

--- index.js
+++ index.js
@@ -9,4 +9,11 @@ app.get('/', (req, res) => {
     res.send('OK')
 })
 
+app.post('/fail', (req, res) => {
+    res.send('fail')
+})
+
+app.post('/notfail', (req, res) => {
+    res.send('notfail')
+})
 app.listen(process.env.PORT || 8080, () => console.log(`All OK`))
