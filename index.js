d966a4fac1d211d1ce4ca44d5c08294a45067ac4
Koshkens <33977229+Koshkens@users.noreply.github.com>
2020-03-12 20:36:33 +0300

    for heroku

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const express = require('express')
 const app = express()
-const port = (3000 || process.env.PORT);
 const clientUrl = '*';
 
-app.get('/', (req, res) => {
-    res.send("Hello")
-})
 
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err)
-    }
-    console.log(`Server is listening on ${port}`)
-})
+app.use(express.static("public"))
+
+app.listen(8080 || process.env.PORT, () => console.log(`Server is listening on ${port}`))
